083552b1bce9722badbbdb936f2dd5d4357b1d07
Nicolas <nicolas.7l@hotmail.com>
2020-10-02 09:42:18 -0300

    App select Pizza

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-community/picker';
 
 export default function App() {
+
+  const [pizza, setPizza] = useState([
+    { id: 1, nome: 'Frango com catupiry', valor: '35,90' },
+    { id: 2, nome: 'Calabresa com Queijo', valor: '45,90' },
+    { id: 3, nome: 'Frango com Cheddar', valor: '30,90' },
+    { id: 4, nome: 'Moda da Casa', valor: '25,90' },
+  ])
+  const [pizzaSelect, setPizzaSelect] = useState ([{ id: 0, nome: '', valor: '00,00' }])
+
+ function selectPizza(item) {
+    setPizzaSelect(pizza[item])
+   
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.logo}>Menu Pizza</Text>
+
+      <Picker
+      //selectedValue={}
+       onValueChange={(itemValue, itemIndex) => selectPizza(itemIndex)}
+      >
+        {pizza.map(i => <Picker.Item key={i.id} value={i.valor} label={i.nome} />)}
+
+      </Picker>
+
+      <Text style={styles.pizzas}>Voce escolheu: {pizzaSelect.nome} </Text>
+       <Text style={styles.pizzas}>R$ {pizzaSelect.valor}</Text>
     </View>
   );
 }
@@ -18,4 +43,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold'
+
+  },
+  pizzas: {
+    marginTop: 15,
+    fontSize: 25,
+    textAlign: 'center',
+  }
 });
